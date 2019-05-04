4042b1e03119863aa79d839e45452fb050d81902
ditch0 <ditch0@users.noreply.github.com>
2019-05-04 19:23:25 +0300

    Use class methods

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -10,18 +10,21 @@ class Robot(object):
         self.reset()
 
     def reset(self) -> None:
-        self.name = self.__generate_unique_name()
+        self.name = Robot.__generate_unique_name()
 
-    def __generate_unique_name(self) -> str:
+    @classmethod
+    def __generate_unique_name(cls) -> str:
         while True:
-            name = self.__generate_name()
-            if name not in self.used_names:
-                self.used_names.add(name)
+            name = cls.__generate_name()
+            if name not in cls.used_names:
+                cls.used_names.add(name)
                 return name
 
-    def __generate_name(self) -> str:
-        return self.__generate_random_string(string.ascii_uppercase, 2) + self.__generate_random_string(string.digits, 3)
+    @classmethod
+    def __generate_name(cls) -> str:
+        return cls.__generate_random_string(string.ascii_uppercase, 2) + cls.__generate_random_string(string.digits, 3)
 
-    def __generate_random_string(self, chars: str, length: int) -> str:
+    @classmethod
+    def __generate_random_string(cls, chars: str, length: int) -> str:
         random_chars = random.choices(chars, k=length)
         return ''.join(random_chars)
