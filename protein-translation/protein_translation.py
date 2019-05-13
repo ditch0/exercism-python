6ebfcd28059c7db889dbddd0bca01c997a391ce5
ditch0 <ditch0@users.noreply.github.com>
2019-05-13 12:47:22 +0300

    Solve Protein Translation

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,29 @@
+import itertools
+from typing import List
+
+CODONS_NAMES = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+}
+
+
+def split_string_to_chunks(string: str, chunk_length: int) -> List[str]:
+    return [string[i:i + chunk_length] for i in range(0, len(string), chunk_length)]
+
+
+def proteins(strand: str) -> List[str]:
+    codons = split_string_to_chunks(strand, 3)
+    translatable_codons = itertools.takewhile(lambda codon: codon in CODONS_NAMES, codons)
+    return [CODONS_NAMES.get(codon) for codon in translatable_codons]
