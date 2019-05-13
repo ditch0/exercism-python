b5dd38d4dbec897f25da99c93849089e443d4046
ditch0 <ditch0@users.noreply.github.com>
2019-05-13 16:05:35 +0300

    Fix code style

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -3,4 +3,3 @@ def distance(strand_a: str, strand_b: str) -> int:
         raise ValueError('Strands must have same length')
 
     return len([i for i in range(len(strand_a)) if strand_a[i] != strand_b[i]])
-
