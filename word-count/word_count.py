f5b5f077deda40a968e7217566b27899ceccebef
ditch0 <ditch0@users.noreply.github.com>
2019-05-14 08:51:31 +0300

    Improve type annotations

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -3,10 +3,10 @@ import re
 from typing import Dict
 
 
-def word_count(phrase: str) -> Dict:
+def word_count(phrase: str) -> Dict[str, int]:
     words = re.findall(r'\d+|[a-z]+(?:\'[a-z]+)?', phrase.lower(), flags=re.IGNORECASE)
 
-    def count_word(counts: Dict, word: str) -> Dict:
+    def count_word(counts: Dict[str, int], word: str) -> Dict[str, int]:
         prev_count = counts.get(word, 0)
         counts[word] = prev_count + 1
         return counts
