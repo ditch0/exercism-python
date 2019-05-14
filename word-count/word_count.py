cf739007c8742c60ee23b52a976525c22fdc6e82
ditch0 <ditch0@users.noreply.github.com>
2019-05-14 08:42:18 +0300

    Solve Words count

---
+++ word-count/word_count.py
@@ -0,0 +1,14 @@
+import functools
+import re
+from typing import Dict
+
+
+def word_count(phrase: str) -> Dict:
+    words = re.findall(r'\d+|[a-z]+(?:\'[a-z]+)?', phrase.lower(), flags=re.IGNORECASE)
+
+    def count_word(counts: Dict, word: str) -> Dict:
+        prev_count = counts.get(word, 0)
+        counts[word] = prev_count + 1
+        return counts
+
+    return functools.reduce(count_word, words, {})
