77b5ee7d6ceaf5bb9416d83813358c116f2812fd
ditch0 <ditch0@users.noreply.github.com>
2019-05-17 12:55:15 +0300

    Update Prime factors

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -5,17 +5,14 @@ def is_divisible(number: int, divisor: int) -> bool:
     return number % divisor == 0
 
 
-def prime_factors(natural_number: int) -> List[int]:
+def prime_factors(number: int) -> List[int]:
     factors = []
     divisor = 2
-    quotient = natural_number
-    while True:
-        if is_divisible(quotient, divisor):
+    while number > 1:
+        if is_divisible(number, divisor):
             factors.append(divisor)
-            quotient = quotient // divisor
+            number //= divisor
         else:
             divisor += 1
-        if quotient == 1:
-            break
 
     return factors
