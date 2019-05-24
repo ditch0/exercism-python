de0bbd9908d84ef2e7f014c450ce57af024c8714
ditch0 <ditch0@users.noreply.github.com>
2019-05-24 13:42:57 +0300

    Update Sum of multiples

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,23 +1,10 @@
-import itertools
 from typing import List
 
 
-def multiples_of(number: int, limit: int) -> List[int]:
-    if number == 0:
-        return [0]
-
-    iterator = itertools.takewhile(
-        lambda n: n < limit,
-        itertools.count(number, number)
-    )
-    return list(iterator)
-
-
 def sum_of_multiples(limit: int, numbers: List[int]) -> int:
-    multiples = [
-        multiples_of(number, limit)
-        for number
-        in numbers
-    ]
-    unique_multiples = set(itertools.chain(*multiples))
-    return sum(unique_multiples)
+    return sum({
+        multiple
+        for number in numbers
+        if number > 0
+        for multiple in range(number, limit, number)
+    })
