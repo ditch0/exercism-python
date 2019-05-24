1b68f761d47c65f8afaf219fa1b43b40342ef07e
ditch0 <ditch0@users.noreply.github.com>
2019-05-24 08:59:08 +0300

    Update Run length encoding

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,41 +1,26 @@
+import itertools
 import re
-from typing import List
 
 
 def decode(string: str) -> str:
+    matches = re.findall(r'(\d*)(\D)', string)
     decoded_groups = [
         character * int(digits or 1)
         for digits, character
-        in re.findall(r'(\d*)(\D)', string)
+        in matches
     ]
     return ''.join(decoded_groups)
 
 
 def encode(string):
-    char_groups = _split_to_repeating_character_groups(string)
-    encoded_groups = [_encode_repeating_character_group(group) for group in char_groups]
+    groups = [
+        (character, len(list(group)))
+        for character, group
+        in itertools.groupby(string)
+    ]
+    encoded_groups = [
+        f'{count if count > 1 else ""}{character}'
+        for character, count
+        in groups
+    ]
     return ''.join(encoded_groups)
-
-
-def _split_to_repeating_character_groups(string: str) -> List[str]:
-    groups = []
-    prev_char = None
-    group = ''
-    for char in string:
-        if char == prev_char or prev_char is None:
-            group += char
-        else:
-            groups.append(group)
-            group = char
-        prev_char = char
-    else:
-        if group:
-            groups.append(group)
-
-    return groups
-
-
-def _encode_repeating_character_group(group: str) -> str:
-    if len(group) == 1:
-        return group
-    return '{}{}'.format(len(group), group[0])
